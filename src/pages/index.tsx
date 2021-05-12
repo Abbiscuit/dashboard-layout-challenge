5077edf1cbb086dd1c2dd5ab6d46b407a5aa81a8
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2021-05-12 14:06:30 +0900

    Add app button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
+import { TutorialButton } from '../components/app/TutorialButton';
 import { Typography } from '../components/ui/Typography';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="h-screen dark:bg-gray-800">
       <h1>Home Page</h1>
       <Typography.H1>こんにちは！</Typography.H1>
       <Typography.H2>こんにちは！</Typography.H2>
       <Typography.H3>こんにちは！</Typography.H3>
       <Typography.Paragraph>こんにちは！</Typography.Paragraph>
+
+      <TutorialButton />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,36 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+      primary: {
+        50: '#FEE5ED',
+        100: '#FECCDC',
+        200: '#FD9AB9',
+        300: '#FB6997',
+        400: '#FA3774',
+        500: '#F80652',
+        600: '#C60542',
+        700: '#940431',
+        800: '#630221',
+        900: '#310110',
+      },
+    },
+
+    truncate: {
+      lines: {
+        2: '2',
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [
+    // require('@tailwindcss/aspect-ratio'),
+    // require('tailwindcss-truncate-multiline')(),
+  ],
 };
