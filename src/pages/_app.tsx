400648ef03f641b4cc12fd82d4402b1b701eb380
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2021-05-20 14:14:14 +0900

    data normalization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <header className="fixed top-0 left-0 right-0 flex flex-row w-full bg-gray-50">
+        <Link href={`/view-and-logic`}>ViewLogic</Link>
+      </header>
       <Component {...pageProps} />
     </>
   );
